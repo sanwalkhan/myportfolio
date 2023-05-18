bb07e64b2c637cbe47656e225599d3c0000ea193
Sanwal Khan <71782878+sanwalkhan@users.noreply.github.com>
2023-05-19 01:32:03 +0500

    main

--- thissanwal/app/(site)/page.tsx
+++ thissanwal/app/(site)/page.tsx
@@ -6,12 +6,12 @@ export default async function Home() {
   const projects = await getProjects();
 
   return (
-    <div className="max-w-5xl mx-auto py-20 pl-8">
+    <div >
       <h1 className="text-7xl font-extrabold">
         Hello I&apos;m
         <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
           {" "}
-          Sanwal Khan!
+          Sanwal!
         </span>
       </h1>
       <p className="mt-3 text-xl text-gray-600">
